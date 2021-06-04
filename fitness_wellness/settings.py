76f80d312f3680d1bf8e7493b050a1f0b1d6d155
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-04 10:41:17 +0000

    Added membership app and template

--- fitness_wellness/settings.py
+++ fitness_wellness/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'gym',
+    'membership',
 ]
 
 MIDDLEWARE = [
--- membership/views.py
+++ membership/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def membership(request):
+    """ A view to return our gym page"""
+
+    return render(request, 'membership/membership.html')
