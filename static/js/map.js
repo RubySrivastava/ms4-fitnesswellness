8a57d4c73f43d51943fa8ffed3eaaa1ce901e3f4
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-08 09:32:21 +0000

    Delete blank lines

--- static/js/map.js
+++ static/js/map.js
@@ -3,11 +3,9 @@ function initMap() {
         lat: 57.659012,
         lng: 11.990340
     }; /*Möndal*/
-
-
+    
     /*Select Location*/
     /*Display Map & Marker on map*/
-
     const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: myLatLong,
