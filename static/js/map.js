f5fe55e03d90317d9c65b7d13e3ef31adc9ef490
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-08 08:36:34 +0000

    Add map and js in contact page

---
+++ static/js/map.js
@@ -0,0 +1,17 @@
+function initMap() {
+    const myLatLong = { lat: 57.659012, lng: 11.990340 }; /*Möndal*/
+    
+
+    /*Select Location*/
+    /*Display Map & Marker on map*/
+    
+        const map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: myLatLong,
+        });
+        new google.maps.Marker({
+            position: myLatLong,
+            map,
+        });
+}
+    
