aa35c02ed59dcda5d4fd0572624118ad03a8a506
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-15 09:10:14 +0000

    Created subscriber form

---
+++ subscribe/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SubscriberForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True, widget=forms.TextInput(attrs={'class':'form-label'})
+    )
