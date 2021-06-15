7265847568ff2935ad86bbfbe82e9339d5f92aa7
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-15 10:16:21 +0000

    Update view

--- subscribe/views.py
+++ subscribe/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
 from .forms import SubscriberForm
+from .models import Subscribe
 
 
 def subscribe(request, template='subscribe/subscribe.html'):
