a992a42e46bbba2701941932338e264f9d71d3c9
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-15 09:19:51 +0000

    Added subscribe view

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import SubscriberForm
 
 
-def subscribe(request):
-    """ A view to return the index page """
+def subscribe(request, template='subscribe/subscribe.html'):
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            # Unpack form values
+            email = form.cleaned_data['email']
+            # Create the User record
+            user = User(email=email)
+            user.save()     
+    else:
+        form = SubscriberForm()
 
-    return render(request, 'subscribe/subscribe.html')
+    return render(request, template, {'form':form})
