c64bde0483c4a7cbcb05848d03a09b37bd6eb42b
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-16 14:20:33 +0000

    Created testimonial view

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import TestimonialForm
+
+
+def all_testimonials(request):
+    """ A view to show all testimonials """
+
+    testimonials = Testimonial.objects.all()
+    
+    context = {
+        'testimonials': testimonials,
+    }
+
+    return render(request, 'testimonials/testimonials.html', context)
+
+
