a60634cf5d529282145437c8a17af689739788d3
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-16 09:56:50 +0000

    Added testimonial view

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import TestimonialForm
 
-def contact(request):
-    """ A view to return the index page """
 
-    return render(request, 'contact/contact.html')
+def add_testimonial(request, template='contact/contact.html'):
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added testimonial!')
+        else:
+            messages.error(request, 'Failed to add testimonial')
+    else:
+        form = TestimonialForm()
+
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+    
