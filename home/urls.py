83c7f3feb6b7a8a04e7f0bc99f3caf36d80c389e
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-18 11:15:58 +0000

    Added testimonial view,url,admin and model in home app

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    
+    path('', views.all_testimonials, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
 
-# Create your views here.
+from .models import Testimonial
 
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def all_testimonials(request):
+    """ A view to show all testimonials """
+
+    testimonials = Testimonial.objects.all()
+
+    context = {
+        'testimonials': testimonials,
+    }
+
+    return render(request, 'home/index.html', context)
