24db63f1e64de3fd0a0d0c5d1913d30c3267b48c
RubySrivastava <ruby.srivas1984@gmail.com>
2021-06-18 12:22:13 +0000

    Delete testimonila link from contact page and delete testimonial app

--- testimonials/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-
-from .models import Testimonial
-
-class TestimonialForm(forms.ModelForm):
-    class Meta:
-        model = Testimonial
-        fields = '__all__'
--- testimonials/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.all_testimonials, name='testimonials'),
-    path('add/', views.add_testimonial, name='add_testimonial'),
-]
